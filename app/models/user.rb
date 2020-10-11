5599982ba454ce492f20b3b341c2075169a5e9dd
Keita Mac <ktnshzm@gmail.com>
2020-10-11 11:28:45 +0900

    set regex formats and validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,38 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  #全カラム必須とする
+  with_options presence: true do
+    validates :nickname
+    validates :email
+    validates :password
+    validates :first_name
+    validates :first_name_kana
+    validates :last_name
+    validates :last_name_kana
+    validates :birth_date
+  end
+
+  #ユーザー情報のvalidation
+  EMAIL_REGEX = /.+@.+/.freeze
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]{6}+\z/i.freeze
+
+  validates :email, uniqueness: true
+  validates_format_of :email, with: EMAIL_REGEX, message: 'には@を含めて設定してください'
+  validates_format_of :password, with: PASSWORD_REGEX, message: 'には英字と数字の両方を含めて設定してください'
+
+  #本人情報確認のvalidation
+  REALNAME_REGEX = /\A[ぁ-んァ-ン一-龥]+\z/
+  KANA_REGEX = /\A[ァ-ン]+\z/
+
+  with_options format: { with: REALNAME_REGEX, message: '全角文字を使用してください' } do
+    validates :first_name
+    validates :last_name
+  end
+
+  with_options format: { with: KANA_REGEX, message: '全角カタカナを使用してください' } do
+    validates :first_name_kana
+    validates :last_name_kana
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_08_205020) do
+ActiveRecord::Schema.define(version: 2020_10_08_204551) do
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -18,14 +18,14 @@ ActiveRecord::Schema.define(version: 2020_10_08_205020) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
+    t.string "nickname", null: false
+    t.string "first_name", null: false
+    t.string "first_name_kana", null: false
+    t.string "last_name", null: false
+    t.string "last_name_kana", null: false
+    t.date "birth_date", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.string "nickname"
-    t.string "first_name"
-    t.string "first_name_kana"
-    t.string "last_name"
-    t.string "last_name_kana"
-    t.date "birth_date"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
