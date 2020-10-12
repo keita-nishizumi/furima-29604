4c53d74aaa8d1167fbee2bd6b697d9d632eaa4fa
Keita Mac <ktnshzm@gmail.com>
2020-10-12 17:21:13 +0900

    added validations for item model

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,15 @@
 class Item < ApplicationRecord
   belongs_to :user
+
+  with_options presence: true do
+    validates :user
+    validates :name
+    validates :information_text
+    validates :category_id
+    validates :status_id
+    validates :shipping_fee_status_id
+    validates :prefecture_id
+    validates :scheduled_delivery_id
+    validates :sell_price
+  end
 end
