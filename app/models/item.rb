7aa05d506846e7f7342ad76130a6428d937cdb45
Keita Mac <ktnshzm@gmail.com>
2020-10-14 06:07:23 +0900

    created categories and modified item model validation so that user must other data than 0

--- app/models/item.rb
+++ app/models/item.rb
@@ -6,11 +6,11 @@ class Item < ApplicationRecord
     validates :user
     validates :name, length: { maximum: 40 }
     validates :information_text, length: { maximum: 1000 }
-    validates :category_id
-    validates :status_id
-    validates :shipping_fee_status_id
-    validates :prefecture_id
-    validates :scheduled_delivery_id
+    validates :category_id, numericality: { other_than: 0 }
+    validates :status_id, numericality: { other_than: 0 }
+    validates :shipping_fee_status_id, numericality: { other_than: 0 }
+    validates :prefecture_id, numericality: { other_than: 0 }
+    validates :scheduled_delivery_id, numericality: { other_than: 0 }
     validates :sell_price, numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999 }
   end
 
